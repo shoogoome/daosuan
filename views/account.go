2d9417a2941526198abbae6bbc744a0d542cd1b8
121 <shoogoome@sina.com>
2020-01-28 14:30:03 +0800

    add 添加账户路由

--- views/account.go
+++ views/account.go
@@ -1,13 +1,25 @@
 package views
 
 import (
+	"daosuan/views/account"
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/hero"
+
 	//"daosuan/views/account"
 )
 
 func RegisterAccountRouters(app *iris.Application) {
 
 	// 账户路由
-	//accountRouter := app.Party("/accounts")
-	//accountRouter
+	accountRouter := app.Party("/accounts")
+
+	accountRouter.Post("", hero.Handler(account.MgetAccountInfo))
+	accountRouter.Get("/list", hero.Handler(account.GetAccountList))
+	accountRouter.Post("/_mget", hero.Handler(account.MgetAccountInfo))
+	accountRouter.Post("/register", hero.Handler(account.Register))
+	accountRouter.Get("/login/check", hero.Handler(account.CheckLogin))
+	accountRouter.Get("/logout", hero.Handler(account.Logout))
+	accountRouter.Post("/login/common", hero.Handler(account.CommonLogin))
+	accountRouter.Get("/{aid:int}", hero.Handler(account.GetAccount))
+	accountRouter.Put("/{aid:int}", hero.Handler(account.PutAccount))
 }
