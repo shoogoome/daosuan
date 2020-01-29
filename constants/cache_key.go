9704787639bca66e9fc1821f9e01f59c4e43bae6
121 <shoogoome@sina.com>
2020-01-29 11:34:16 +0800

    add 添加缓存键

--- constants/cache_key.go
+++ constants/cache_key.go
@@ -1,8 +1,23 @@
 package constants
 
-// 存储key
+// 缓存key
 const (
 
+	// 数据库缓存键
 	DbModel = "model"
+
+	// 昵称缓存键
+	NicknameModel = "account:nickname"
+
+	// 关注缓存键
+	FollowingModel = "account:following"
+
+	FollowerModel = "account:follower"
+
+	// 用户点赞缓存键
+	AccountStarModel = "account:star"
+
+	// 点赞缓存键
+	StarModel = "product:star"
 
 )
