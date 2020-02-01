5bed2f90a045986f1be55f7874f81ef5258d4a54
121 <shoogoome@sina.com>
2020-02-01 11:14:29 +0800

    change 修改全局资源路由

--- views/resource.go
+++ views/resource.go
@@ -11,6 +11,6 @@ func RegisterResourceRouters(app *iris.Application) {
 	// 资源路由
 	resourceRouter := app.Party("/resources")
 
-	resourceRouter.Get("/{token:string}", hero.Handler(resource.LocalDownload))
+	resourceRouter.Get("/local/{token:string}", hero.Handler(resource.LocalDownload))
 	resourceRouter.Get("/qiniu/upload_token", hero.Handler(resource.GetQiNiuUploadToken))
 }
