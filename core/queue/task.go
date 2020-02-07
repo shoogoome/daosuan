25a5f424824c26c1ff08814e45b33445e1ea5781
121 <shoogoome@sina.com>
2020-02-07 23:55:15 +0800

    change

--- core/queue/task.go
+++ core/queue/task.go
@@ -18,7 +18,7 @@ func InitTaskQueue() {
 			function := <-Task
 			startTime := time.Now()
 			logUtils.Println("启动任务")
-			logUtils.Println(fmt.Sprintf("当前任务队列等待数%d", len(Task)))
+			logUtils.Println(fmt.Sprintf("当前任务队列: %d/%d", len(Task), utils.GlobalConfig.Server.TaskQueueLength))
 			function()
 			logUtils.Println(fmt.Sprintf("任务结束，耗时: %s", time.Now().Sub(startTime).String()))
 		}
