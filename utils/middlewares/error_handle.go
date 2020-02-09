d08603df68d49fb6d307518896ed80ccbef739e3
121 <shoogoome@sina.com>
2020-02-09 17:57:53 +0800

    add 异常处理器添加打印堆栈功能

--- utils/middlewares/error_handle.go
+++ utils/middlewares/error_handle.go
@@ -1,31 +1,42 @@
 package middlewares
 
 import (
+	"daosuan/models"
 	"fmt"
 	"github.com/kataras/iris"
-	"daosuan/models"
+	"runtime"
 )
 
+// 异常控制器
 func AbnormalHandle(ctx iris.Context) {
 	defer func() {
 		re := recover()
 		if re == nil {
 			return
 		}
+		ctx.StatusCode(iris.StatusInternalServerError)
+		// 打印堆栈信息
+		if debug, err := ctx.URLParamInt("debug"); err == nil && debug == 1 {
+			ctx.Text(stack())
+			return
+		}
+		// 输出api格式反馈
 		switch result := re.(type) {
 		case models.RestfulAPIResult:
-			ctx.StatusCode(iris.StatusInternalServerError)
 			ctx.JSON(result)
 		default:
-			ctx.StatusCode(iris.StatusInternalServerError)
 			ctx.JSON(models.RestfulAPIResult{
 				Status: false,
 				ErrCode: 500,
-				Message: fmt.Sprintf("%v", re),
+				Message: fmt.Sprintf("系统错误: %v", result),
 			})
-			panic(re)
 		}
 	}()
 	ctx.Next()
+}
+
+func stack() string {
+	var buf [2 << 10]byte
+	return string(buf[:runtime.Stack(buf[:], true)])
 }
 
