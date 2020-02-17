2a023f42d966c9f5e100ce45d44774f3fea9d145
121 <shoogoome@sina.com>
2020-02-17 19:27:07 +0800

    log

--- views/system/config.go
+++ views/system/config.go
@@ -8,7 +8,7 @@ import (
 
 // 重载配置
 func HeavyLoadConfig(ctx iris.Context, auth authbase.DaoSuanAuthAuthorization) {
-	//auth.CheckAdmin()
+	auth.CheckAdmin()
 	utils.InitGlobal()
 	ctx.JSON(iris.Map {
 		"status": "重载成功",
